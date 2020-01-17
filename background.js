a53ebd88f7bca0bd57a0038941743b188511228c
kaustubh.m <kaustubh.m>
2020-01-18 00:16:35 +0530

    working on fetching json file data

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
-chrome.runtime.onInstalled.addListener(function() {
-    console.log('Plugin installed');
-});
+// Varaiblec required
+chrome.runtime.onInstalled.addListener(function() {});
+
+// // Access config file
+// function fetchData() {
+//     let url = chrome.runtime.getURL(filePath);
+// }
+// const configData = chrome.runtime.getURL('./data/config.json');
